0bc3413ffb6a6282f6bcd85358febef156166ea5
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-03-23 02:50:26 +0300

    commit_statistic_instantData_With_SignalIR

--- SignalRApi/Hubs/SignalIRHub.cs
+++ SignalRApi/Hubs/SignalIRHub.cs
@@ -1,17 +1,73 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalIR.BusinessLayer.Abstract;
 using SignalIR.DataAccessLayer.Concrete;
 
 namespace SignalRApi.Hubs
 {
     public class SignalIRHub : Hub
     {
-        SignalIRContext context = new SignalIRContext();
+        private readonly ICategoryService _categoryService;
+        private readonly IProductService _productService;
+        private readonly IOrderService _orderService;
+        private readonly IMoneyCaseService _moneyCaseService;
+        private readonly IMenuTableService _menuTableService;
 
-        public async Task SendCategoryCount()
+        public SignalIRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, IMoneyCaseService moneyCaseService, IMenuTableService menuTableService)
         {
-            var value = context.Categories.Count();
-
-            await Clients.All.SendAsync("ReceiveCategoryCount", value);
+            _categoryService = categoryService;
+            _productService = productService;
+            _orderService = orderService;
+            _moneyCaseService = moneyCaseService;
+            _menuTableService = menuTableService;
         }
+
+        public async Task SendStatistic()
+        {
+            var categoryCount = _categoryService.TCategoryCount();
+            await Clients.All.SendAsync("ReceiveCategoryCount", categoryCount);
+
+            var productCount = _productService.TGetProductCount();
+            await Clients.All.SendAsync("ReceiveProductCount", productCount);
+
+            var activeCategoryCount = _categoryService.TActiveCategoryCount();
+            await Clients.All.SendAsync("ReceiveActiveCategoryCount", activeCategoryCount);
+
+            var passiveCategoryCount = _categoryService.TPassiveCategoryCount();
+            await Clients.All.SendAsync("ReceivePassiveCategoryCount", passiveCategoryCount);
+
+            var productCountByCategoryNameHamburger = _productService.TProductCountByCategoryNameHamburger();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameHamburger", productCountByCategoryNameHamburger);
+
+            var productCountByCategoryNameDrink = _productService.TProductCountByCategoryNameDrink();
+            await Clients.All.SendAsync("ReceiveProductCountByCategoryNameDrink", productCountByCategoryNameDrink);
+
+            var highestPricedProductName = _productService.THighestPricedProductName();
+            await Clients.All.SendAsync("ReceiveHighestPricedProductName", highestPricedProductName);
+
+            var lowestPricedProductName = _productService.TLowestPriceProductName();
+            await Clients.All.SendAsync("ReceiveLowestPricedProductName", lowestPricedProductName);
+
+            var productPriceAvarage = _productService.TProductPriceAvg();
+            await Clients.All.SendAsync("ReceiveProductPriceAvarage", productPriceAvarage.ToString("0.00" + " ₺"));
+
+            var productPriceByHamburgerAvarage = _productService.TProductPriceByHamburgerAvg();
+            await Clients.All.SendAsync("ReceiveProductPriceByHamburgerAvg", productPriceByHamburgerAvarage.ToString("0.00" + " ₺"));
+
+            var totalOrderCount = _orderService.TTotalOrderCount();
+            await Clients.All.SendAsync("ReceiveTotalOrderCount", totalOrderCount);
+
+            var activeOrderCount = _orderService.TActiveOrderCount();
+            await Clients.All.SendAsync("ReceiveActiveOrderCount", activeOrderCount);
+
+            var lastOrderPrice = _orderService.TLastOrderPrice();
+            await Clients.All.SendAsync("ReceiveLastOrderPrice", lastOrderPrice.ToString("0.00" + " ₺"));
+
+            var totalMoneyCaseAmount = _moneyCaseService.TtotalMoneyCaseAmount();
+            await Clients.All.SendAsync("ReceiveTotalMoneyCaseAmount", totalMoneyCaseAmount.ToString("0.00" + " ₺"));
+
+            var menuTableCount = _menuTableService.TMenuTableCount();
+            await Clients.All.SendAsync("ReceiveMenuTableCount", menuTableCount);
+        }
+
     }
 }
