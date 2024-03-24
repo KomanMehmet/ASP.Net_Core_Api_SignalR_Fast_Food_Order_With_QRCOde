f4cf03240d555aba4ad3c296a9682806bebe805c
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-03-24 22:46:14 +0300

    commit_menuPageUIComsume&sweetAlery

---
+++ SignalRWebUI/Controllers/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.ProductDtos;
+
+namespace SignalRWebUI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MenuController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.GetAsync("https://localhost:7074/api/Products");
+
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+                var value = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+
+                return View(value);
+            }
+
+            return View();
+        }
+    }
+}
