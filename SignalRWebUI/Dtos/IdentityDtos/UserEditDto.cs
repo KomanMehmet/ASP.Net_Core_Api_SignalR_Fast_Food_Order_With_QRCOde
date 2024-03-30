24ae0ad954f7059d51a3cebb2ba7eb592904dc2a
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-03-30 22:54:44 +0300

    commit_userSettings

---
+++ SignalRWebUI/Dtos/IdentityDtos/UserEditDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SignalRWebUI.Dtos.IdentityDtos
+{
+    public class UserEditDto
+    {
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ComfirmPassword { get; set; }
+    }
+}
